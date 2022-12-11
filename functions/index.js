5ee4c65c97fb9b8fa10c496bb809d3d7e2ec39f5
Jacques Laguerre <jacquesblaguerre@gmail.com>
2022-12-11 10:19:08 -0500

    updates

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
 import functions from "firebase-functions";
-import { addNewExercise, getAllExercise,  addReview, getAllReview, updateReview, deleteReview } from "./src/exercise.js";
+import { addNewExercise, getAllExercise,  addReview, getAllReview, updateReview, deleteReview, updateExercise, addReviewAcl, getAllAclReview, 
+  addReviewHamstring, getAllHamstringReview, addReviewAnkle, getAllAnkleReview
+} from "./src/exercise.js";
 
 
 const app = express();
@@ -10,7 +12,7 @@ app.use(cors());
 
 app.post('/exercise', addNewExercise)
 app.get("/exercise", getAllExercise);
-// app.patch('/exercise/:exerciseId', updateExercise)
+app.patch('/exercise/:exerciseId', updateExercise)
 // app.get('/exercise/:exerciseId', getOneExercise)
 
 app.post('/review', addReview)
@@ -18,4 +20,13 @@ app.get('/review', getAllReview)
 app.patch('/review',updateReview )
 app.delete('/review', deleteReview)
 
+app.post('/reviewAcl', addReviewAcl)
+app.get('/reviewAcl', getAllAclReview)
+
+app.post('/reviewHamstring', addReviewHamstring)
+app.get('/reviewHamstring', getAllHamstringReview)
+
+app.post('/reviewAnkle', addReviewAnkle)
+app.get('/reviewAnkle', getAllAnkleReview)
 export const api = functions.https.onRequest(app);
+
--- functions/src/exercise.js
+++ functions/src/exercise.js
@@ -20,21 +20,80 @@ export async function getAllExercise(req, res) {
   res.send(collection);
 }
 
- 
-
-export async function deleteExercise (req, res ){
-  const db = dbConnect ()
-  const  {exerciseId} = req.params
-  const collection = await db.collection("exercise")
-  .findOneAndDelete ({_id: new ObjectId (exerciseId)})
-  res.status(203).send('Exercise deleted')
+export async function deleteExercise(req, res) {
+  const db = dbConnect();
+  const { exerciseId } = req.params;
+  const collection = await db
+    .collection("exercise")
+    .findOneAndDelete({ _id: new ObjectId(exerciseId) });
+  res.status(203).send("Exercise deleted");
 }
 
-export async function addReview(req, res) {
+
+
+export async function updateExercise(req, res) {
+  const { exerciseId } = req.params;
+  const db = dbConnect();
+  
+  await db
+  .collection("exercise")
+  .findOneAndUpdate({ _id: new ObjectId(exerciseId) }, { $set: req.body })
+  .catch((err) => {
+    res.status(500).send(err);
+    return;
+  });
+  getAllExercise(req, res);
+}
+
+
+  // ---------REVIEWS------------
+  export async function addReview(req, res) {
+    const newReview = req.body;
+    const db = dbConnect();
+    await db
+      .collection("review")
+      .insertOne(newReview)
+      .catch((err) => {
+        res.status(500).send(err);
+        return;
+      });
+    res.status(201).send({ message: "Review Added." });
+  }
+  
+  export async function getAllReview(req, res) {
+    const db = dbConnect();
+    const collection = await db.collection("review").find().toArray();
+    res.send(collection);
+  }
+  
+  export async function updateReview(req, res) {
+    const { reviewId } = req.params;
+    const db = dbConnect();
+  
+    await db
+      .collection("review")
+      .findOneAndUpdate({ _id: new ObjectId(reviewId) }, { $set: req.body })
+      .catch((err) => {
+        res.status(500).send(err);
+        return;
+      });
+    res.status(202).send({ message: "review updated" });
+  }
+  
+  export async function deleteReview(req, res) {
+    const db = dbConnect();
+    const { reviewId } = req.params;
+    const collection = await db
+      .collection("exercise")
+      .findOneAndDelete({ _id: new ObjectId(reviewId) });
+    res.status(203).send("review deleted");
+  }
+// ------------ACL REVIEWS-----------
+export async function addReviewAcl(req, res) {
   const newReview = req.body;
   const db = dbConnect();
   await db
-    .collection("review")
+    .collection("reviewAcl")
     .insertOne(newReview)
     .catch((err) => {
       res.status(500).send(err);
@@ -43,41 +102,50 @@ export async function addReview(req, res) {
   res.status(201).send({ message: "Review Added." });
 }
 
-export async function getAllReview(req, res) {
+export async function getAllAclReview(req, res) {
   const db = dbConnect();
-  const collection = await db.collection("review").find().toArray();
+  const collection = await db.collection("reviewAcl").find().toArray();
   res.send(collection);
 }
 
-export async function updateReview(req, res) {
-  const { reviewId } = req.params;
+// ---------Hamstring Reviews---------
+export async function addReviewHamstring(req, res) {
+  const newReview = req.body;
   const db = dbConnect();
-
   await db
-    .collection("review")
-    .findOneAndUpdate({ _id: new ObjectId(reviewId) }, { $set: req.body })
+    .collection("reviewHamstring")
+    .insertOne(newReview)
     .catch((err) => {
       res.status(500).send(err);
       return;
     });
-  res.status(202).send({ message: "review updated" });
+  res.status(201).send({ message: "Review Added." });
 }
 
-export async function deleteReview(req, res) {
+export async function getAllHamstringReview(req, res) {
   const db = dbConnect();
-  const { reviewId } = req.params;
-  const collection = await db
-    .collection("exercise")
-    .findOneAndDelete({ _id: new ObjectId(reviewId) });
-  res.status(203).send("review deleted");
+  const collection = await db.collection("reviewHamstring").find().toArray();
+  res.send(collection);
+}
+// ---------Ankle---------
+export async function addReviewAnkle(req, res) {
+  const newReview = req.body;
+  const db = dbConnect();
+  await db
+    .collection("reviewAnkle")
+    .insertOne(newReview)
+    .catch((err) => {
+      res.status(500).send(err);
+      return;
+    });
+  res.status(201).send({ message: "Review Added." });
+}
+
+export async function getAllAnkleReview(req, res) {
+  const db = dbConnect();
+  const collection = await db.collection("reviewAnkle").find().toArray();
+  res.send(collection);
 }
 
 
-
-// export async function findExerciseByType(req,res) {
-//   const db = dbConnect()
-//   const { search } = req.params
-//   const collection = await db.collection("furniture").find({ type: search }).toArray()
-//   res.send(collection)
-// }
 
